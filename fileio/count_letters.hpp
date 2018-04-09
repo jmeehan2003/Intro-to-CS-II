4e745c3644ec5fd689ef97f4c90de47127dd05a4
James Meehan <ghoyas2003@gmail.com>
2018-04-08 20:21:58 -0400

    Create count_letters.hpp

---
+++ fileio/count_letters.hpp
@@ -0,0 +1,12 @@
+/****************************************
+** Author: James Meehan
+** Description: This is the header file
+** for the count_letters function
+****************************************/
+
+#ifndef COUNT_LETTERS_HPP
+#define COUNT_LETTERS_HPP
+
+void count_letters(ifstream &, int*);
+
+#endif
